9c9d560af1d61d4254b61cc660c73fb640873c62
Arina <arinadwi14@gmail.com>
2024-05-30 21:18:20 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+const typed = new Typed('.multiple-text', {
+    strings: ['am Fresh Graduate', 'Want to be Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
